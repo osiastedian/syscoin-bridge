4afe4b4986ca7f10cd00caff6f8dc6a74d4a90ca
Ted Ian Osias <osiastedian@gmail.com>
2023-11-14 22:12:48 +0800

    feat(admin): add session login

--- constants.ts
+++ constants.ts
@@ -1,8 +1,10 @@
 export const MIN_AMOUNT = 0.001;
-export const DEFAULT_GAS_LIMIT = 120_000
+export const DEFAULT_GAS_LIMIT = 120_000;
 
 export const RELAY_CONTRACT_ADDRESS =
   "0xD822557aC2F2b77A1988617308e4A29A89Cb95A6";
 
 export const SYSCOIN_TX_BLOCKCHAIN_URL = "https://blockbook.elint.services/tx/";
 export const NEVM_TX_BLOCKCHAIN_URL = "https://explorer.syscoin.org/tx/";
+
+export const ADMIN_LOGIN_MESSAGE = "Login to Syscoin Bridge Admin";
